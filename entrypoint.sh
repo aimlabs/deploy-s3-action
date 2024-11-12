eabc40353f9212b2454c9a2f7715959a5741f301
krishnachaitanya <chaitanya415@gmail.com>
2024-11-12 18:11:35 +0530

    added OVERWIRTE_CONTENT

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
 #Sync files with amazon s3 bucket app
-aws --region $INPUT_AWS_DEFAULT_REGION s3 sync $DIST_LOCATION_CODE s3://$INPUT_AWS_BUCKET_NAME$INPUT_BUCKET_PATH --no-progress
+if [$INPUT_OVERWIRTE_CONTENT=='true' || $INPUT_OVERWIRTE_CONTENT=='TRUE'] 
+then
+    aws --region $INPUT_AWS_DEFAULT_REGION s3 sync $DIST_LOCATION_CODE s3://$INPUT_AWS_BUCKET_NAME$INPUT_BUCKET_PATH --no-progress --delete
+else    
+    aws --region $INPUT_AWS_DEFAULT_REGION s3 sync $DIST_LOCATION_CODE s3://$INPUT_AWS_BUCKET_NAME$INPUT_BUCKET_PATH --no-progress 
+fi
