a4f67ca9abd0e2d38b867f14ab3a37aeaaeec2cd
krishnachaitanya <chaitanya415@gmail.com>
2024-11-12 20:53:36 +0530

    test OVERWIRTE_CONTENT

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 #Sync files with amazon s3 bucket app
 echo "Am here buddy $INPUT_OVERWIRTE_CONTENT"
 
-if [$INPUT_OVERWIRTE_CONTENT=='true' || $INPUT_OVERWIRTE_CONTENT=='TRUE'] 
+if [$INPUT_OVERWIRTE_CONTENT == "true" || $INPUT_OVERWIRTE_CONTENT == "TRUE"] 
 then
     echo "Am here buddy TRUE"
     aws --region $INPUT_AWS_DEFAULT_REGION s3 sync $DIST_LOCATION_CODE s3://$INPUT_AWS_BUCKET_NAME$INPUT_BUCKET_PATH --no-progress --delete
